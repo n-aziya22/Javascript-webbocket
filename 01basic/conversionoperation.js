3e1ecb7662401db1c1f73b1d2fe0b5e41206be80
n-aziya22 <152247449+n-aziya22@users.noreply.github.com>
2023-11-30 11:22:11 +0000

    fileadded

---
+++ 01basic/conversionoperation.js
@@ -0,0 +1,6 @@
+let Score=33
+//let Score=null;
+//let Score="gift";
+//let Score=true;
+console.log(typeof Score);
+console.log(typeof (Score));
---
+++ 01basic/datatype.js
@@ -0,0 +1,10 @@
+let name="manoranjan";
+let age=23;
+let ispresent=true;
+let ID =Symbol('123');
+let AnotherID =Symbol('123');
+
+   console.log(typeof name);
+   console.log(typeof age);
+   console.log(typeof ispresent);
+   console.log(ID===AnotherID);
