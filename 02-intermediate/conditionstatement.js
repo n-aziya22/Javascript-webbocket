3fbe9153d4cc309560b5626e338197e83d84fbae
n-aziya22 <152247449+n-aziya22@users.noreply.github.com>
2023-12-09 05:32:06 +0000

    add file

---
+++ 02-intermediate/conditionstatement.js
@@ -0,0 +1,103 @@
+// condition statement
+
+
+/* condition statement are  those who creates condition on whather you goes this side you goes another side like that
+1.if -if condition true then statemen is true otherwise it is false.
+
+    if(condition)
+    {
+        statement
+    }
+          
+    var a=12;
+    if(a<18)
+    {
+    console.log("they did not give vote");
+    }
+
+2.if-else  -if condition is satisfied then if block is executed otherwise else block will be executed.
+    if(condition)
+    {
+        statement
+    }
+    else{
+        statement
+    }
+
+
+        var a=19;
+    if(a%2==0){
+    console.log("even")
+
+    }
+    else{
+    console.log("odd");
+    }
+
+3.if else-if else-if else -in that we add multiple case simultaneously to check the condition where we get executed.
+    if(condition){
+        statement
+    }
+    else if{
+        statement
+    }
+    else if{
+        statement
+    }
+    else{
+        statement
+    }
+
+                const balance=700;
+if(balance< 500){
+    console.log("less than 500");
+}
+else if(balance <700){
+    console.log("less than 700");
+}
+else if(balance<900){
+    console.log("less than 900");
+}
+else{
+    console.log("less than 1200");
+}
+
+4.switch- a part of javascript conditional statement where we execute different statement depend on the condition that they check.
+
+switch(key){
+    case :value
+    break;
+    case :value
+    break;
+    case:default
+    break;
+}
+
+    const month=7;
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+        case 2:
+            console.log(february);
+            break;
+            case 3:
+                console.log("march");
+                break;
+                case 4:
+                    console.log("april");
+                    break;
+                    case 5:
+                        console.log("may");
+                        break;
+                        case 6:
+                            console.log("june");
+                            break;
+                            default:
+                                console.log("default case")
+                                break;
+                            }
+
+ */
+
+
---
+++ 02-intermediate/loop.js
@@ -0,0 +1,70 @@
+// loops- repeatedly execute the statement
+/* 
+--in javascript there are 5 types of loops are there
+1.for loop
+    for(intialization;condition;incre/decre){
+        statement
+    } 
+2.while loop
+    while(condition){
+        staement;
+        incre/decre;
+    }
+3.do-while loop
+    do{
+    statement;
+    incre/decre;
+    }
+    while(condition)
+
+            var a=12;
+        do{
+    console.log("hello mano");
+    a++;
+        }
+        while(a<15)
+   
+4.for-in loop
+
+--for in loop was only work on object
+--they iterate the key value pair in object.
+
+        
+var obj={
+    name:"mano",
+    age:"22",
+    place:"bbsr"
+}
+for(var gift in obj){
+    console.log(gift);
+    console.log(obj[gift]);
+    console.log(gift,obj[gift]);
+}
+
+5.for-each loop
+
+--for each loop will only execute with an array.
+--its ,ain motive isnbtraverse or iterate the array element one by one.
+
+
+    var a=[1,2,3,4,5,6,7,8,9]
+a.forEach(function(val){
+    console.log(val);
+})
+    */
+
+// var a=12;
+// while(a<20){
+//     console.log(a);
+//    a++;
+// }
+
+
+
+
+
+
+
+
+
+
